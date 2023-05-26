0f01a5d49e77186810db00cf07ecc219959ccc38
james <astonjames@iahni.com>
2023-05-25 23:51:37 -0600

    src test complete

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,6 +1,10 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
+function Header(props) {
+  return <h1>hello from the Header!</h1>;
+}
+export default Header;
 
 import Header from "../components/Header";
 
